9305e65f42d967d36f39955c0aebbbcebd142647
[saroj adhikari] <sawroz.adhikari@gmail.com>
2023-09-25 22:18:05 +0545

    add

--- src/Cards.js
+++ src/Cards.js
@@ -10,6 +10,7 @@ function Cards () {
        <p>Fri 14 Dec 2020</p>
        <p>This is Nepal and <br></br>
        its very beautiful</p>
+       <button className='btn'>Buy tickets</button>
        </div>
     <div className='card1'>
     <img className='image' src='https://images.unsplash.com/photo-1543872084-c7bd3822856f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dG93bnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60'></img>
@@ -17,6 +18,7 @@ function Cards () {
        <p>Fri 14 Dec 2020</p>
        <p>This is Nepal and <br></br>
        its very beautiful</p>
+       <button className='btn'>Buy tickets</button>
        </div>
     <div className='card1'>
     <img className='image' src='https://images.unsplash.com/photo-1543872084-c7bd3822856f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dG93bnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60'></img>
@@ -24,6 +26,7 @@ function Cards () {
      <p>Fri 14 Dec 2020</p>
      <p>This is Nepal and <br></br>
      its very beautiful</p>
+     <button className='btn'>Buy tickets</button>
      </div>
     </div>
     </>
