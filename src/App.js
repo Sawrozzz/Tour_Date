3bd838befd464fb2aae855afa38ac16076890e14
[saroj adhikari] <sawroz.adhikari@gmail.com>
2023-09-27 16:30:17 +0545

    commit new update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Card from "./Cards";
 import Peoples from "./data"
 import month_data from "./data2"
+import { Key } from "@mui/icons-material";
 
 
 function App() {
@@ -12,18 +13,18 @@ function App() {
       <div className="main">
         <Header />
         {
-          month_data.map((e)=>{
+          month_data.map((e,index)=>{
             return(
-              <Month  month_name={e.month_name} isSold ={e.isSold} />
+              <Month key = {index} month_name={e.month_name} isSold ={e.isSold}   />
             )
           })
         }
        
       <div className="card">
       {
-        Peoples.map((elem)=>{
+        Peoples.map((elem, id)=>{
           return(
-            <Card name={elem.name} des ={elem.des} date = {elem.date} image_source = {elem.image_source}/>
+            <Card key={id} name={elem.name} des ={elem.des} date = {elem.date} image_source = {elem.image_source}/>
           )
         })
       }
---
+++ src/eg.js
@@ -0,0 +1,8 @@
+// const arr = ["Saroj", "Arun", "Riya"];
+// const [hi, hello, whatap] = arr;
+
+// console.log(hi);
+// console.log(hello);
+
+//Destructuring Objects 
+
