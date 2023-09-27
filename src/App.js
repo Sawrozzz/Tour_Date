3b93978e06ed2ba952c484046b193ee82a9a3326
[saroj adhikari] <sawroz.adhikari@gmail.com>
2023-09-27 13:22:56 +0545

    add new updates

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Header from "./Header";
 import Card from "./Cards";
 import image1 from "../src/jungle.jpg";
 import image2 from "../src/river.jpg";
+import image from "../src/nepal.jpg";
+import Peoples from "./data"
 
 function App() {
-  let image_url =
-    "https://media.istockphoto.com/id/637696304/photo/patan.jpg?s=612x612&w=0&k=20&c=-53aSTGBGoOOqX5aoC3Hs1jhZ527v3Id_xOawHHVPpg=";
   return (
     <>
       <div className="main">
@@ -19,24 +19,13 @@ function App() {
         <Month isSold={false} month_name ="December"/>
         <hr></hr>
       <div className="card">
-        <Card className="mycard"
-          name="Nepal"
-          date="sun 14 2012"
-          des="Nepal is a beautiful country"
-          image_source={image_url}
-        />
-        <Card
-          name="Jungle"
-          date="Mon 14 2010"
-          des="Jungle are beautifull green and source of oxygen"
-          image_source={image1}
-        />
-        <Card
-          name="River"
-          date="Tue 13 2015"
-          des="Rivers flows like how our life flows "
-          image_source={image2}
-        />
+      {
+        Peoples.map((elem)=>{
+          return(
+            <Card name={elem.name} des ={elem.des} date = {elem.date} image_source = {elem.image_source}/>
+          )
+        })
+      }
       </div>
 
       </div>
---
+++ src/data.jsx
@@ -0,0 +1,26 @@
+import image1 from "../src/jungle.jpg";
+import image2 from "../src/river.jpg";
+import image from "../src/nepal.jpg";
+
+const Peoples = [{
+
+    name :"Nepal",
+    date : "2013-05-23",
+    des : "Nepal is a beautiful country",
+    image_source : image
+},
+{
+    name :"Jungle",
+    date : "2013-08-22",
+    des : "Jungle gungle mangle",
+    image_source : image1
+
+},
+{
+    name: "River",
+    date: "2015 - 04-13",
+    des: "River flows like how our life flows",
+    image_source : image2
+}
+]
+export default Peoples
